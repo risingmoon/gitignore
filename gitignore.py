74143d96c734275af72d79eba8f6dc26cfa79615
Justin Lee <justindavidlee88@gmail.com>
2020-06-07 01:05:01 -0700

    Add python and jetbrains support

--- gitignore.py
+++ gitignore.py
@@ -1,8 +1,43 @@
 #!/usr/bin/env python3
+import argparse
+import pathlib
+
+
+def parse(snippet):
+    parts = snippet.split(':')
+    path = pathlib.Path(__file__).parent / 'snippets'
+
+    folder = parts.pop(0)
+    folder_path = path / folder
+    assert folder_path.exists() is True, '%s is not supported' % folder
+    paths = [path / folder / 'default.txt']
+
+    for part in parts:
+        file = '%s.txt' % part
+        file_path = path / folder / file
+        assert file_path.exists() is True, '%s submodule is not supported' % part
+        paths.append(file_path)
+    return paths
+
+
+def generate(file_paths):
+    content = []
+    for path in file_paths:
+        with open(path, 'r') as f:
+            content.append(f.read())
+    return content
 
 
 def main():
-    print('Hello World')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('snippets', type=str, nargs='+')
+    args = parser.parse_args()
+    file_paths = []
+    for snippet in args.snippets:
+        file_paths.extend(parse(snippet))
+    content = generate(file_paths)
+    with open('.gitignore', 'w') as f:
+        f.writelines('\n\n'.join(content))
 
 
 if __name__ == '__main__':

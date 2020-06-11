c1c420701ad235344cff849cca4cc6250140c71d
Justin Lee <justindavidlee88@gmail.com>
2020-06-10 17:43:41 -0700

    Add Django snippet

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='gitignore-risingmoon',  # Replace with your own username
-    version='0.0.1',
+    version='0.0.2',
     author='Justin Lee',
     author_email='justindavidlee88@gmail.com',
     description='A simple gitignore generator',
